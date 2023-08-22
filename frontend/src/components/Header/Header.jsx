06e5becfa212fdb5db8d1a7970702950a7dac1b1
Kuttan69 <emmanuelpoulose@gmail.com>
2023-08-22 23:04:52 +0000

    earth

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import {ReactNavbar} from "overlay-navbar"
 import {FaUserAlt} from "react-icons/fa"
+import logo from "../../Images/logo.png"
+import homenav from "../../Images/homenav.jpeg"
 
 const Header = () => {
   return (
     <ReactNavbar 
-    navColor1="white" 
-    navColor2="grey"
-    burgerColor="black"
-    burgerColorHover="darkgrey"
+    navColor1="darkgrey" 
+    navColor2="black"
+    burgerColor="grey"
+    burgerColorHover="grey"
     nav2justifyContent="space-around"
     nav3justifyContent="space-around"
     logoWidth="250px"
+    logo={logo}
     link1Text="Home"
-    link2Text="About"
-    link3Text="Projects"
-    link4Text="Contact Me"    
+    link2Text="Sun"
+    link3Text="Planets"
+    link4Text="About Me"  
     link1Url="/home"
-    link2Url="/about"
-    link3Url="/projects"
-    link4Url="/contact"
+    link2Url="/sun"
+    link3Url="/planet"
+    link4Url="/about"
     link1ColorHover="white"
     link1Color="darkgrey"
     link1Size="1.5rem"
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 import sunImage from "../../Images/sun.jpg"
 import mercuryImage from "../../Images/mercury.jpg"
 import venusImage from "../../Images/venus.jpg"
+import earthImage from "../../Images/earth.jpg"
+
 
 const Home = () => {
   useEffect(() => {
@@ -21,10 +23,11 @@ const Home = () => {
     const textureLoader1=new THREE.TextureLoader();
     const textureLoader2=new THREE.TextureLoader();
     const textureLoader3=new THREE.TextureLoader();
+    const textureLoader4=new THREE.TextureLoader();
 
 
     const sunTexture=textureLoader1.load(sunImage); 
-    const sunGeometry=new THREE.SphereGeometry(10,64,64);
+    const sunGeometry=new THREE.SphereGeometry(100,64,64);
     const sunMaterial=new THREE.MeshBasicMaterial({map:sunTexture});
     const sun=new THREE.Mesh(sunGeometry,sunMaterial);
 
@@ -33,43 +36,55 @@ const Home = () => {
     const mercuryGeometry=new THREE.SphereGeometry(4,64,64);
     const mercuryMaterial=new THREE.MeshStandardMaterial({map:mercuryTexture});
     const mercury=new THREE.Mesh(mercuryGeometry,mercuryMaterial);
-    const mercuryAxisGeometry = new THREE.RingGeometry( 70, 70.5, 200 ); 
+    const mercuryAxisGeometry = new THREE.RingGeometry( 205, 205.5, 200 ); 
     const mercuryAxisMaterial = new THREE.MeshBasicMaterial( { color: 0xa1a1a1, side: THREE.DoubleSide } );
     const mercuryAxis = new THREE.Mesh( mercuryAxisGeometry, mercuryAxisMaterial );
 
     const venusTexture=textureLoader3.load(venusImage);
-    const venusGeometry=new THREE.SphereGeometry(4,64,64);
+    const venusGeometry=new THREE.SphereGeometry(10,64,64);
     const venusMaterial=new THREE.MeshStandardMaterial({map:venusTexture});
     const venus=new THREE.Mesh(venusGeometry,venusMaterial);
-    const venusAxisGeometry = new THREE.RingGeometry( 100, 100.5, 200 ); 
+    const venusAxisGeometry = new THREE.RingGeometry( 300, 300.5, 200 ); 
     const venusAxisMaterial = new THREE.MeshBasicMaterial( { color: 0xa1a1a1, side: THREE.DoubleSide } );
     const venusAxis = new THREE.Mesh( venusAxisGeometry, venusAxisMaterial );
     
+    const earthTexture=textureLoader4.load(earthImage);
+    const earthGeometry=new THREE.SphereGeometry(12,64,64);
+    const earthMaterial=new THREE.MeshStandardMaterial({map:earthTexture});
+    const earth=new THREE.Mesh(earthGeometry,earthMaterial);
+    const earthAxisGeometry = new THREE.RingGeometry( 400, 400.5, 200 ); 
+    const earthAxisMaterial = new THREE.MeshBasicMaterial( { color: 0xa1a1a1, side: THREE.DoubleSide } );
+    const earthAxis = new THREE.Mesh( earthAxisGeometry, earthAxisMaterial );
     
-    const pointLight=new THREE.PointLight(0xffffff,10000);
+    const pointLight=new THREE.PointLight(0xEBE0D0,100000);
     pointLight.position.set(0,0,0);
     const lightHelper=new THREE.PointLightHelper(pointLight);
     const controls=new OrbitControls(camera,renderer.domElement);
       
     sun.position.set(0,0,0); 
-    mercury.position.set(50,0,50); 
+    mercury.position.set(200,0,50); 
     mercuryAxis.position.set(0,0,0);  
-    venus.position.set(100,0,0);
-    venusAxis.position.set(0,0,0);
+    venus.position.set(300,0,0);
+    venusAxis.position.set(0,0,0); 
+    earth.position.set(350,0,200);
+    earthAxis.position.set(0,0,0);
 
     mercuryAxis.rotation.set(1.55,0,0);
     venusAxis.rotation.set(1.55,0,0);
+    earthAxis.rotation.set(1.55,0,0);
 
+
+    scene.add(controls);
     scene.add(sun);
     scene.add(mercury);
     scene.add( mercuryAxis );
     scene.add(venus);
     scene.add(venusAxis);
+    scene.add(earth);
+    scene.add(earthAxis);
     scene.add(pointLight); 
     scene.add(lightHelper);
-
-    scene.add(controls);
-    camera.position.set(0,0,200);
+    camera.position.set(0,0,400);
     const animate = () =>{
       requestAnimationFrame(animate);
       mercury.rotation.y+=0.01;
