be23854e5c526f44fedc96e23e3db48dbe0705be
Kuttan69 <emmanuelpoulose@gmail.com>
2023-08-27 21:47:13 +0000

    mongoatlas3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Sun from './components/Sun/Sun';
 import Planets from './components/Planet/Planets';
 import Planet from './components/Planet/Planett';
 import Header from './components/Header/Header';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import {BrowserRouter as Route,Routes} from "react-router-dom"
 function App() {
   return (
     <>
